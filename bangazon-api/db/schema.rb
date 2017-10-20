812b8c1858a68bfdcf20251ac20eb63a7a04d37f
Tyler Barnett <tyler.barnett7190@gmail.com>
2017-10-20 15:57:09 -0500

    added all tables for internal part of Bangazon

--- bangazon-api/db/schema.rb
+++ bangazon-api/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171020154721) do
+ActiveRecord::Schema.define(version: 20171020205346) do
+
+  create_table "computers", force: :cascade do |t|
+    t.date "decommission_date"
+    t.date "purchase_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
@@ -20,6 +27,50 @@ ActiveRecord::Schema.define(version: 20171020154721) do
     t.boolean "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "departments", force: :cascade do |t|
+    t.float "expense_budget"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employee_computers", force: :cascade do |t|
+    t.integer "computer_id"
+    t.integer "employee_id"
+    t.date "start"
+    t.date "end"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["computer_id"], name: "index_employee_computers_on_computer_id"
+    t.index ["employee_id"], name: "index_employee_computers_on_employee_id"
+  end
+
+  create_table "employee_programs", force: :cascade do |t|
+    t.integer "employee_id"
+    t.integer "program_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employee_id"], name: "index_employee_programs_on_employee_id"
+    t.index ["program_id"], name: "index_employee_programs_on_program_id"
+  end
+
+  create_table "employee_types", force: :cascade do |t|
+    t.string "employee_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "employee_type_id"
+    t.integer "department_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["department_id"], name: "index_employees_on_department_id"
+    t.index ["employee_type_id"], name: "index_employees_on_employee_type_id"
   end
 
   create_table "order_products", force: :cascade do |t|
@@ -68,6 +119,14 @@ ActiveRecord::Schema.define(version: 20171020154721) do
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_products_on_customer_id"
     t.index ["product_type_id"], name: "index_products_on_product_type_id"
+  end
+
+  create_table "training_programs", force: :cascade do |t|
+    t.date "start_date"
+    t.integer "max_occupancy"
+    t.date "end_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
